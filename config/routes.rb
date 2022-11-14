c7b5847176532abf5437b25b4106a0caeb0fdc64
mwpenn94 <mwpenn94@gmail.com>
2022-11-14 10:10:30 -0700

    preliminary squeeze page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'contact_us', to: 'pages#contact_us'
+  get 'free', to: 'pages#free'
   get 'sample_deal', to: 'pages#sample_deal'
   get 'about', to: 'pages#about'
   get 'privacy_policy', to: 'pages#privacy_policy'
